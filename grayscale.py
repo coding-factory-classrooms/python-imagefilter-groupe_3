d169449e8d3947eadb3568c20b3d685d9d85405f
Lucas <liger.lucas33@gmail.com>
2020-12-01 16:52:38 +0100

    grayscale modification

--- grayscale.py
+++ grayscale.py
@@ -1,11 +1,15 @@
 import cv2
+import os
 
 def grayscayle(src):
-    img = cv2.imread(src)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Gray image', gray)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-src = "imgs/the_witcher.jpg"
-grayscayle(src)
+    try:
+        new_directory = "output"
+        img = cv2.imread(src)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('Gray image', gray)
+        os.chdir(new_directory)
+        cv2.imwrite("gray_img.jpg", gray)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    except cv2.error :
+        print("Erreur de saisie : le fichier n'existe pas")
