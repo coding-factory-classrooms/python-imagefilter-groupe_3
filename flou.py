a55874b44436486de75c8d5caa71f3f14e1d1ed7
Tristan Laine <tristan.laine@edu.itescia.fr>
2020-12-02 10:36:00 +0100

    Flou 3

--- flou.py
+++ flou.py
@@ -1,11 +1,26 @@
 import cv2
+import logger
+import os
 
 def blur(src):
-    img = cv2.imread(src)
-    blur_image = cv2.GaussianBlur(img, (3,33),0)
-    cv2.imshow('Blurred Image', blur_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    try:
+        new_directory = "output"
+        img = cv2.imread(src)
+        blur_image = cv2.GaussianBlur(img, (3,33),0)
+        logger.log("image : " + src + " floutée")
+        cv2.imshow('Blurred Image', blur_image)
+        logger.log("image : " + src + " ajoutée au dossier : " + new_directory)
+        os.chdir(new_directory)
+        cv2.imwrite("blured_img.jpg", blur_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    except cv2.error :
+        print ("Erreur de saisie du chemin")
 
-src = "imgs/schtroumpfette.jpg"
-blur(src)
+path = "imgs"
+dirs = os.listdir(path)
+
+for file in dirs:
+    print(file)
+    blur(f'imgs/{file}')
+
