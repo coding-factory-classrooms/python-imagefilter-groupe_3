600187b0e6492373c2789ec74dcae69887418837
Lucas <liger.lucas33@gmail.com>
2020-12-03 15:45:13 +0100

    enfin.

---
+++ imread.py
@@ -0,0 +1,5 @@
+import cv2
+
+def imread(src):
+    img = cv2.imread(src)
+    return img
--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import filter_selection
 import os
 import sys
 import writing
+import imread
+import cv2
 
+dico_img = {}
 args = sys.argv
-new_directory="output"
-list_filtre=[]
+new_directory = "output"
+list_filtre = []
 
-#étape 1: initialiser ma config à partir de la CLI
+# étape 1: initialiser ma config à partir de la CLI
 try:
     print(args)
     if args[1] == "--filters":
@@ -16,7 +19,7 @@ try:
             filter_ls = filter_ls.split("|")
             for n in filter_ls:
                 effect_ls = n.split(":")
-                if len(effect_ls)>1 :
+                if len(effect_ls) > 1:
                     filter = {"filter_type": effect_ls[0], "filter_value": effect_ls[1]}
                     list_filtre.append(filter)
                     if filter["filter_type"] == "dilate":
@@ -25,29 +28,47 @@ try:
                     elif filter["filter_type"] == "blur":
                         print("filtre blur")
                 else:
-                    filter = {"filter_type": effect_ls[0]}
+                    filter = {"filter_type": effect_ls[0], "filter_value": 0}
                     list_filtre.append(filter)
                     if filter["filter_type"] == "grayscale":
                         print("filtre grayscale")
 
 
-    elif args[1]=="-h":
+    elif args[1] == "-h":
         print("Voici les différentes commandes :")
-except IndexError :
+except IndexError:
     print("erreur d'index : arguments manquant. Tappez \"-h\" pour de l'aide")
 
-#étape 2: exécuter mon programme à partir de la config
+# étape 2: exécuter mon programme à partir de la config
 path = "imgs"
+i = 0
 dirs = os.listdir(path)
-for file in dirs:
-    print(file)
-    for filter_dict in list_filtre:
-        print(len(filter_dict))
-        if len(filter_dict)>1:
-            filter_selection.filter_application(filter_dict["filter_type"], int(filter_dict["filter_value"]), f"{path}/{file}", file)
-        else :
-            filter_selection.filter_application(filter_dict["filter_type"], 0,
-                                                f"{path}/{file}", file)
-        writing.write_file(new_directory, file, )
 
-print(list_filtre)
+for file in dirs:
+    if file.find(".png") >= 0 or file.find(".jpg") >= 0 or file.find(".jpeg"):
+
+        print(file)
+        try:
+            # Ouvrir image :
+            dico_img[i] = imread.imread(f"{path}/{file}")
+            print(dico_img[i])
+            print(f"{path}/{file}")
+            for filter_dict in list_filtre:
+                # Appliquer les filtres :
+
+                try:
+                    dico_img[i] = filter_selection.filter_application(filter_dict["filter_type"],
+                                                                      int(filter_dict["filter_value"]), file,
+                                                                      dico_img[i])
+                    print("filtre appliquer")
+                except cv2.error:
+                    print("erreur filtre")
+
+        # Ecrire l'image :
+        except AttributeError:
+            print("oui")
+        try:
+            writing.write_file(new_directory, file, dico_img[i])
+            i = i + 1
+        except cv2.error:
+            print("erreur 1")
